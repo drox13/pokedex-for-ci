571a4a09f302287e130e6c3536dbde8cfce7077e
drox13 <dario.baron@uptc.edu.co>
2022-06-26 18:56:24 -0500

    despliege heroku

--- .github/workflows/cypress/integration/app.js
+++
@@ -1,9 +0,0 @@
-/*global cy */
-
-describe("Pokedex", function(){
-    it("frompage can be opened", function(){
-        cy.visit("http//localhost:5000")
-        cy.constains("picachu")
-        cy.constains("Pokémon and Pókemon character names are trademarks of Nintendo")
-    })
-})
